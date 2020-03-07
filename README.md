21658b101e242344382a0ac684468a0cf1eb7aa7
RunS WD <anair8793@gmail.com>
2020-03-06 21:55:28 -0600

    done

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# homework-10
+# Homework-10
+
+## Employee Summary: Template Engine
+
+## Description:
+
+This assignment focused on creating a team's basic information in a web page.
+
+```
+As a manager
+I want to generate a webpage that displays my team's basic info
+so that I have quick access to emails and GitHub profiles
+```
+
+## Project Link
+
+[Homework-10](./app.js)
+
+## Instructions
+
+The app is run from the command line after an npm install by the user. After following all prompts and providing necessary information, the app will generate an html page with all relevant info.
+
+
+## Images
+
+![Node](https://user-images.githubusercontent.com/57499384/76136205-bade2100-5ff4-11ea-8365-378981161011.png)
+
+![HTML](https://user-images.githubusercontent.com/57499384/76136222-e103c100-5ff4-11ea-9e8e-49a94246f9ea.png)
--- app.js
+++ app.js
@@ -4,14 +4,13 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
+
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+
 const render = require("./lib/htmlRenderer");
-​
-​const teamMember = [];
-const idArray = [];
+
+const teamMembers = [];
 
 function appMenu() {
 
@@ -21,29 +20,28 @@ function appMenu() {
             {
                 type: "input",
                 name: "managerName",
-                message: "What is your manager's name?",
+                message: "What is your manager's name?"
             },
             {
                 type: "input",
                 name: "managerId",
-                message: "What is your manager's Id?",
+                message: "What is your manager's Id?"
             },
             {
                 type: "input",
                 name: "managerEmail",
-                message: "What is your manager's email?",
+                message: "What is your manager's email?"
             },
             {
                 type: "input",
                 name: "managerOfficeNumber",
-                message: "What is your manager's office number?",
+                message: "What is your manager's office number?"
             }
         ]).then(answers => {
             const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
-            teamMember.push(manager);
-            idArray.push(answers.managerId);
+            teamMembers.push(manager);
             createTeam();
-        })
+        });
     }
 
     function createTeam() {
@@ -52,7 +50,7 @@ function appMenu() {
             {
                 type: "list",
                 name: "memberChoice",
-                message: "Which type of team member would you like to add?"
+                message: "Which type of team member would you like to add?",
                 choices: ["Engineer", "Intern", "No additional team members"]
             }
         ]).then(userChoice => {
@@ -69,8 +67,73 @@ function appMenu() {
         });
     }
 
+    function addEngineer() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "What is your engineer's name?"
+            },
+            {
+                type: "input",
+                name: "engineerId",
+                message: "What is your engineer's Id?"
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "What is your engineer's email?"
+            },
+            {
+                type: "input",
+                name: "engineerGithub",
+                message: "What is your engineer's GitHub username?"
+            }
+        ]).then(answers => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+            teamMembers.push(engineer);
+            createTeam();
+        });
+    }
 
+    function addIntern() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "internName",
+                message: "What is your intern's name?"
+            },
+            {
+                type: "input",
+                name: "internId",
+                message: "What is your intern's Id?"
+            },
+            {
+                type: "input",
+                name: "internEmail",
+                message: "What is your intern's email?"
+            },
+            {
+                type: "input",
+                name: "internSchool",
+                message: "What is your intern's School?"
+            }
+        ]).then(answers => {
+            const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+            teamMembers.push(intern);
+            createTeam();
+        });
+    }
 
+    function buildTeam() {
+        if (!fs.existsSync(OUTPUT_DIR)) {
+            fs.mkdirSync(OUTPUT_DIR)
+        }
+        fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+    }
 
+    createManager();
 
 }
+
+appMenu();
